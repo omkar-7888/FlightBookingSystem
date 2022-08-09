eec80e269ca962262c6022c10e319240d3f38892
omkar-7888 <101782885+omkar-7888@users.noreply.github.com>
2022-08-09 21:38:01 +0530

    Add files via upload

---
+++ flightbooking_apigateway/src/test/java/com/apidateway/FlightbookingApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apidateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightbookingApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
